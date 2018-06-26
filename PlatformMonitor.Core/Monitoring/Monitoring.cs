866bd74d10b5b201ab41c7eff8863e0f46b6026e
Szymon Żogała <szymek.655@gmail.com>
2018-06-26 17:30:15 +0200

    Implementation finished for now

--- PlatformMonitor.Core/Monitoring/Monitoring.cs
+++ PlatformMonitor.Core/Monitoring/Monitoring.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Text;
 
@@ -8,29 +9,32 @@ namespace PlatformMonitor.Core
     public class Monitoring
     {
 		#region Public properties
-
+		
+		/// <summary>
+		/// List with all currently managed <see cref="MonitoringService"/>s
+		/// </summary>
 		public List<MonitoringService> ManagedServices { get; private set; } = new List<MonitoringService>();
 
-		#endregion
-
-		#region Private methods
-
-		private async void Monitor()
-		{
-			
-		}
+		/// <summary>
+		/// Event raised when one of the managed <see cref="MonitoringService"/>s finds one of its targets
+		/// </summary>
+		public EventHandler<NameSpottedEventArgs> NameSpotted;
 
 		#endregion
 
 		#region Public methods
 
+		/// <summary>
+		/// Creates a new service for monitoring
+		/// </summary>
+		/// <param name="url"></param>
+		/// <param name="name"></param>
+		/// <param name="period"></param>
 		public void CreateService(string url, string name, int period)
 		{
-
+			ManagedServices.Add(new MonitoringService(url, new ObservableCollection<string>() { url }, NameSpotted, 10));
 		}
 
-		#endregion
-
-		
+		#endregion		
 	}
 }
