c2f87a09fbde6ebd55ab744eb24b4c6b98f549ed
Szymon Żogała <szymek.655@gmail.com>
2018-06-26 17:09:14 +0200

    Added

---
+++ PlatformMonitor.Core/Monitoring/NameSpottedEventArgs.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlatformMonitor.Core
+{
+	/// <summary>
+	/// Arguments for an event notifying that someone was spotted
+	/// </summary>
+    public class NameSpottedEventArgs : EventArgs
+    {
+		#region Public properties
+
+		/// <summary>
+		/// Url on which the target was spotted
+		/// </summary>
+		public string Url { get; private set; }
+
+		/// <summary>
+		/// Name of the target
+		/// </summary>
+		public string Name { get; private set; }
+
+		/// <summary>
+		/// Time of spotting
+		/// </summary>
+		public DateTime Time { get; private set; }
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Default Constructor
+		/// </summary>
+		public NameSpottedEventArgs(string url, string name, DateTime time)
+		{
+			Url = url;
+			Name = name;
+			Time = time;
+		}
+
+		#endregion
+	}
+}
