b6ef7d8b11c82a316648283227bb7bd70fa925bc
Szymon Żogała <szymek.655@gmail.com>
2018-06-27 14:42:35 +0200

    Added

---
+++ PlatformMonitor/ValueConverters/BoolToPauseIconConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace PlatformMonitor
+{
+	/// <summary>
+	/// If value is a bool return a play icon for false and pause icon for true. Otherwise returns <see cref="string.Empty"/>
+	/// </summary>
+    public class BoolToPauseIconConverter : IValueConverter
+    {
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if(value is bool b)
+			{
+				return b ? "\uE769" : "\uE768";
+			}
+
+			return string.Empty;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
